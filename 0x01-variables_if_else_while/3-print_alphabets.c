8876bc979b8f26ab5598d119052d4c9779937f5b
zwonaka <zwonakada@gmail.com>
2023-02-17 17:06:08 -0800

    ./3-print_alphabets | cat -e

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
  * main - Entry point
  * Return: Always 0 (success)
  */
 
-
 int main(void)
+
+{
+	char low;
         
-{       
-        char low;
-        
-        for (low = 'a'; low <= 'z'; low++)
-        putchar(low);
-        
-        for (low = 'A'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+			
+	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 	putchar('\n');
 
 	return (0);
 
 }
-
