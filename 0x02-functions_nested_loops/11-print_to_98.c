40d1546952f575c3ebc705c358571a2507f4814e
zwonaka <zwonakada@gmail.com>
2023-02-22 11:59:43 -0800

    ./11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,8 +3,8 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
  * @n: print from this number
+ * Return: Always 0.
  */
 
 void print_to_98(int n)
@@ -41,7 +41,7 @@ void print_to_98(int n)
 	{
 	printf("%d\n", n);
 	}
-	}    
+	}
 	}
 }
 
