52103f18e48ed3d2c0de2b95ad237d326d77eed4
zwonaka <zwonakada@gmail.com>
2023-02-22 11:50:17 -0800

    ./11-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,37 +10,38 @@
 void print_to_98(int n)
 
 {
-        if (n <= 98)
-        {
-        for (; n <= 98; n++)
-        {
-        if (n == 98)
-        {
-        printf("%d, ", n);
-        printf("\n");
-        break;
-        }
-        else
+
+	if (n <= 98)
 	{
-        printf("%d, ", n);
-        }
-        }
-        }
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d, ", n);
+	printf("\n");
+	break;
+	}
 	else
 	{
-        for (; n >= 98; n--)
-        {
-        if (n == 98)
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
 	{
-        printf("%d, ", n);
-        printf("\n");
-        break;
-        }
-        else 
-        {
-        printf("%d\n", n);
-        }
-        }       
-        }
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("%d, ", n);
+	printf("\n");
+	break;
+	}
+	else 
+	{
+	printf("%d\n", n);
+	}
+	}       
+	}
 }
 
